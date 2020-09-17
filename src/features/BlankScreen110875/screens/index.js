5406c218157b6ad226fdfcc66a14d473aff1e4b3
Crowdbotics <team@crowdbotics.com>
2020-09-17 09:01:06 +0000

    Update components

---
+++ src/features/BlankScreen110875/screens/index.js
@@ -0,0 +1,97 @@
+import React from "react"
+import { View, Image, ImageBackground } from "react-native"
+import {
+  withStyles,
+  Text,
+  Button,
+  CheckBox,
+  Radio,
+  Toggle,
+  Icon,
+  Input,
+  Datepicker
+} from "react-native-ui-kitten"
+import Slider from "@react-native-community/slider"
+
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+import { connect } from "react-redux"
+export class __Blank extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+  state = {}
+  render = () => (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        marginLeft: 0,
+        marginRight: 0,
+        marginTop: 0,
+        marginBottom: 0,
+        paddingLeft: 0,
+        paddingRight: 0,
+        paddingTop: 0,
+        paddingBottom: 0,
+        overflow: "visible",
+        fontSize: 12,
+        color: "#000000",
+        backgroundColor: "#ffffff",
+        fontFamily: "Roboto-Regular",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        alignContent: "stretch",
+        borderColor: "#000000",
+        borderStyle: "solid",
+        borderWidth: 0,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+        borderRadius: 0,
+        backgroundSize: "auto"
+      }}
+    >
+      <Image
+        resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/8_AMRrGZX.jpg"
+        }}
+        style={{
+          width: "100%",
+          height: 200,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0
+        }}
+      />
+    </View>
+  )
+}
+
+_Blank = withStyles(__Blank, theme => ({
+  container: { backgroundColor: theme["color-basic-100"] }
+}))
+
+function mapStateToProps(state) {
+  return { state: state }
+}
+const actionCreators = {}
+export default connect(mapStateToProps, actionCreators)(_Blank)
